657b0ded2a98d0a6c15d1549572560bdb22ac0b4
Azlan-Dev <amazonae880@gmail.com>
2026-01-16 15:38:53 +0500

    Create basic_programs.py

---
+++ basic_programs.py
@@ -0,0 +1,26 @@
+# Simple Python Programs
+# Author: Muhammad Azlan Asad
+
+# 1. Print Hello
+print("Hello, Welcome to my Python Portfolio")
+
+# 2. Input and Output
+name = input("Enter your name: ")
+print("Hello", name)
+
+# 3. Addition of two numbers
+a = int(input("Enter first number: "))
+b = int(input("Enter second number: "))
+print("Sum:", a + b)
+
+# 4. Check even or odd
+number = int(input("Enter a number: "))
+if number % 2 == 0:
+    print("Even number")
+else:
+    print("Odd number")
+
+# 5. Simple loop
+print("Numbers from 1 to 5:")
+for i in range(1, 6):
+    print(i)
